d42d7b15c9e84a59d4d2058369e4b87d3d5e54ac
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-18 15:47:02 -0500

    Desafio V

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+var contenedor = document.getElementsByClassName("contenedor")[0];
+	contenedor.style.position = "relative";
+	contenedor.style.background = "DarkGray";
+	contenedor.style.width = "600px";
+	contenedor.style.padding = "50px";
+
+var cajarosa = document.getElementById("rosa");
+var cajaazul = document.getElementById("azul");
+	cajaazul.style.position = "absolute";
+	cajaazul.style.top = "250px";
+	cajaazul.style.right = "0";
+	
+var cajaverde = document.getElementById("verde");
+var cajaamarilla = document.getElementById("amarillo");
